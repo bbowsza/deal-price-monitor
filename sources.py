c63d4b5b11a6f15d90fa7effd3a9395f2d1394f2
bbowsza <bbowsza@gmail.com>
2026-07-24 12:12:20 -0400

    Create sources.py

---
+++ sources.py
@@ -0,0 +1,59 @@
+SOURCES = [
+
+    {
+        "name": "BedJet",
+        "type": "shopify",
+        "url":
+        "https://bedjet.com/products/bedjet-3-climate-comfort-system-with-biorhythm-sleep-technology.js"
+    },
+
+    {
+        "name": "Amazon",
+        "type": "search",
+        "url":
+        "https://www.amazon.com/s?k={query}"
+    },
+
+    {
+        "name": "Best Buy",
+        "type": "search",
+        "url":
+        "https://www.bestbuy.com/site/searchpage.jsp?st={query}"
+    },
+
+    {
+        "name": "Target",
+        "type": "search",
+        "url":
+        "https://www.target.com/s?searchTerm={query}"
+    },
+
+    {
+        "name": "Walmart",
+        "type": "search",
+        "url":
+        "https://www.walmart.com/search?q={query}"
+    },
+
+    {
+        "name": "MorningSave",
+        "type": "search",
+        "url":
+        "https://morningsave.com/search?q={query}"
+    },
+
+    {
+        "name": "GMA Deals",
+        "type": "search",
+        "url":
+        "https://gmadeals.com/search?q={query}"
+    },
+
+    {
+        "name": "Slickdeals",
+        "type": "search",
+        "url":
+        "https://slickdeals.net/newsearch.php?src=SearchBox&q={query}"
+    }
+
+]
