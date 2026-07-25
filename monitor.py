5199e460761ff2b6074975a74d7d5ce27ed08cf8
bbowsza <bbowsza@gmail.com>
2026-07-24 23:51:03 -0400

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -136,13 +136,14 @@ for product in PRODUCTS:
         )
 
 
-        if source["type"] == "shopify":
+       if source["type"] == "shopify":
 
-            result = shopify_price(
-                url
-            )
+    result = shopify_price(
+        url,
+        product
+    )
 
-        else:
+else:
 
             result = search_page(
                 url,
